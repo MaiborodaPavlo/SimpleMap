46c3050daacd2ec0f9450200805741168e5e7b53
Pavel Mayboroda <Successbloger1@gmail.com>
2018-02-01 10:07:11 +0200

    Initial Commit

---
+++ SimpleMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleMap
+//
+//  Created by Pavel on 01.02.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
